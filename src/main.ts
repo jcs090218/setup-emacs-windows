3347ee414c9bcf44f9c4b9c4fe39da8fd3b01565
jenchieh <jcs090218@gmail.com>
2020-08-28 22:02:26 +0800

    Test source.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import * as core from '@actions/core';
-import {wait} from './wait';
+import * as exec from "@actions/exec";
+import * as io from "@actions/io";
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    try {
+        const version = core.getInput("version");
+        const emacsCIVersion = "emacs-" + version.replace(".", "-");
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        core.startGroup("Installing Emacs");
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+        core.endGroup();
+
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 run();
