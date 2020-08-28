ff13557ae78f03c72a81a208ed30752e55265649
jenchieh <jcs090218@gmail.com>
2020-08-29 02:36:01 +0800

    Add path.

--- src/main.ts
+++ src/main.ts
@@ -68,9 +68,7 @@ async function run(): Promise<void> {
             emacsBin = emacsDir + "\\" + emacs_dot_var + "\\bin";
         }
 
-        const cachtedPath = await tc.cacheDir(emacsBin, "emacs", dot_ver);
-        core.addPath(cachtedPath);
-        //core.exportVariable("PATH", "%PATH%;" + emacsBin);
+        core.exportVariable("PATH", "%PATH%;" + emacsBin);
         core.endGroup();
 
     } catch (error) {
