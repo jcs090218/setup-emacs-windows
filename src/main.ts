5ae3fab2548704f2c452c37083a64f4e0d34f7f7
jenchieh <jcs090218@gmail.com>
2020-08-28 23:56:36 +0800

    Update installation of Emacs.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import * as core from '@actions/core';
 import * as exec from "@actions/exec";
 import * as io from "@actions/io";
+import * as tc from "@actions/tool-cache";
 
 async function run(): Promise<void> {
     try {
         const version = core.getInput("version");
-        const emacsCIVersion = "emacs-" + version.replace(".", "-");
+        const emacs_dot_var = "emacs-" + version;
+        const emacs_dash_ver = emacs_dot_var.replace(".", "-");
 
         core.startGroup("Installing Emacs");
+        const ftpUrl = "https://ftp.gnu.org/gnu/emacs/windows/" + emacs_dash_ver + "/";
+        const zipPath = ftpUrl + emacs_dot_var + "-x86_64.zip";
+        const emacsZip = await tc.downloadTool(zipPath);
+        const emacsDir = await tc.extractZip(emacsZip, './');
+
+        const emacsBin = "%PATH%;" + emacsDir + "bin";
+        console.log("emacsBin: " + emacsBin);
+        core.exportVariable("PATH", emacsBin);
 
         core.endGroup();
 
     } catch (error) {
-        core.setFailed(error.message)
+        core.setFailed(error.message);
     }
 }
 
