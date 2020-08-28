1f2df5e78c49eb9c3e37702d4914a98a3c6af12c
jenchieh <jcs090218@gmail.com>
2020-08-29 00:06:20 +0800

    Supply full version.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,37 @@ async function run(): Promise<void> {
 
         core.startGroup("Installing Emacs");
         const ftpUrl = "https://ftp.gnu.org/gnu/emacs/windows/" + emacs_dash_ver + "/";
-        const zipPath = ftpUrl + emacs_dot_var + "-x86_64.zip";
+        let zipPath = ftpUrl + emacs_dot_var;
+
+        switch (emacs_dot_var) {
+            case "23.4":
+            case "24.1":
+            case "24.2":
+            case "24.3":
+                zipPath += "-bin-i386.zip";
+                break;
+            case "24.4":
+                zipPath += "-bin-i686-pc-mingw32.zip";
+                break;
+            case "24.5":
+                zipPath += "-bin-i686-mingw32.zip";
+                break;
+            case "25.1":
+                zipPath += "-x86_64-w64-mingw32.zip";
+                break;
+            case "25.2":
+            case "25.3":
+            case "26.1":
+            case "26.2":
+            case "26.3":
+            case "27.1":
+                zipPath += "-x86_64.zip";
+                break;
+            default:
+                zipPath += "-x86_64.zip";
+                break;
+        }
+
         console.log("zipPath: " + zipPath);
         const emacsZip = await tc.downloadTool(zipPath);
         const emacsDir = await tc.extractZip(emacsZip, './');
