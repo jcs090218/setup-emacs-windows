a5ca968e74e44501b77f18295b683736c0904bc0
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-20 22:41:02 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import * as exec from "@actions/exec";
 import * as io from "@actions/io";
 import * as tc from "@actions/tool-cache";
-import fs from 'fs'
+import fs from 'fs';
 
 async function run(): Promise<void> {
     try {
