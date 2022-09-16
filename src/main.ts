c73e3b8eb5215f0d9e4a9c0758e0e19f7c3399dd
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-16 16:37:39 +0800

    Update snapshot -> 20220914

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function run(): Promise<void> {
         if (version == "snapshot") {
             // NOTE: If snapshot, directly assign the newest version.
             // Current newest snaptshot is `29.0.50`.
-            zipPath = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-29/emacs-29.0.50-snapshot-2022-07-18.zip";
+            zipPath = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-29/emacs-29.0.50-snapshot-2022-09-14.zip";
         } else {
             switch (dot_ver) {
                 case "23.4":
