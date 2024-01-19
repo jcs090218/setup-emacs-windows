2f21b719b6ad84262adc7a276c4d85471bff6947
jcs090218 <jcs090218@users.noreply.github.com>
2024-01-19 06:36:53 +0000

    Update emacs stable

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,8 @@ async function run(): Promise<void> {
                 }
                 case "28.1":
                 case "28.2":
-                case "29.1": {
+                case "29.1": 
+                case "29.2": {
                     zipPath += ".zip";
                     break;
                 }
